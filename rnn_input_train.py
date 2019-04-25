56014411761eb4e9ec98d5b053148211c9f22f6e
JRagbeer <30732548+jragbeer@users.noreply.github.com>
2019-04-25 16:48:38 -0400

    commented

--- rnn_input_train.py
+++ rnn_input_train.py
@@ -1,48 +1,38 @@
 import tensorflow as tf
 from tensorflow.contrib import layers
-from tensorflow.contrib import rnn  # rnn stuff temporarily in contrib, moving back to code in TF 1.1
+from tensorflow.contrib import rnn
 import os
 import time
 import math
 import numpy as np
 import my_txtutils as txt
-tf.set_random_seed(0)
 import datetime
+
+tf.set_random_seed(0)
 timee = datetime.datetime.now()
 
+# ADAPTED FROM 'DEEP LEARNING WITHOUT A PHD' FROM GOOGLE
 # model parameters
-#
-# Usage:
-#   Training only:
-#         Leave all the parameters as they are
-#         Disable validation to run a bit faster (set validation=False below)
-#         You can follow progress in Tensorboard: tensorboard --log-dir=log
-#   Training and experimentation (default):
-#         Keep validation enabled
-#         You can now play with the parameters anf follow the effects in Tensorboard
-#         A good choice of parameters ensures that the testing and validation curves stay close
-#         To see the curves drift apart ("overfitting") try to use an insufficient amount of
-#         training data (shakedir = "shakespeare/t*.txt" for example)
-#
-SEQLEN = 30
+
 BATCHSIZE = 200
 ALPHASIZE = txt.ALPHASIZE
 INTERNALSIZE = 512
 NLAYERS = 3
+SEQLEN = 30
 learning_rate = 0.001  # fixed learning rate
 dropout_pkeep = 0.8    # some dropout
 
-# load data, either shakespeare, or the Python source of Tensorflow itself
-shakedir = "all/*.txt"
-codetext, valitext, bookranges = txt.read_data_files(shakedir, validation=True)
+# load data
+docdir = "all/*.txt"
+code_text, validation_text, bookranges = txt.read_data_files(docdir, validation=True)
 
 # display some stats on the data
-epoch_size = len(codetext) // (BATCHSIZE * SEQLEN)
-txt.print_data_stats(len(codetext), len(valitext), epoch_size)
+epoch_size = len(code_text) // (BATCHSIZE * SEQLEN)
+txt.print_data_stats(len(code_text), len(validation_text), epoch_size)
 
 lr = tf.placeholder(tf.float32, name='lr')  # learning rate
 pkeep = tf.placeholder(tf.float32, name='pkeep')  # dropout parameter
-batchsize = tf.placeholder(tf.int32, name='batchsize')
+batchsize = tf.placeholder(tf.int32, name='batchsize') # batch size parameter
 
 # inputs
 X = tf.placeholder(tf.uint8, [None, None], name='X')    # [ BATCHSIZE, SEQLEN ]
@@ -58,26 +48,24 @@ Hin = tf.placeholder(tf.float32, [None, INTERNALSIZE*NLAYERS], name='Hin')  # [ 
 
 # How to properly apply dropout in RNNs: see README.md
 cells = [rnn.GRUCell(INTERNALSIZE) for _ in range(NLAYERS)]
-# "naive dropout" implementation
-dropcells = [rnn.DropoutWrapper(cell,input_keep_prob=pkeep) for cell in cells]
+# naive dropout
+dropcells = [rnn.DropoutWrapper(each,input_keep_prob=pkeep) for each in cells]
 multicell = rnn.MultiRNNCell(dropcells, state_is_tuple=False)
 multicell = rnn.DropoutWrapper(multicell, output_keep_prob=pkeep)  # dropout for the softmax layer
 
 Yr, H = tf.nn.dynamic_rnn(multicell, Xo, dtype=tf.float32, initial_state=Hin)
-# Yr: [ BATCHSIZE, SEQLEN, INTERNALSIZE ]
-# H:  [ BATCHSIZE, INTERNALSIZE*NLAYERS ] # this is the last state in the sequence
 
-H = tf.identity(H, name='H')  # just to give it a name
+H = tf.identity(H, name='H')
 
 # Softmax layer implementation:
 # Flatten the first two dimension of the output [ BATCHSIZE, SEQLEN, ALPHASIZE ] => [ BATCHSIZE x SEQLEN, ALPHASIZE ]
 # then apply softmax readout layer. This way, the weights and biases are shared across unrolled time steps.
 # From the readout point of view, a value coming from a sequence time step or a minibatch item is the same thing.
 
-Yflat = tf.reshape(Yr, [-1, INTERNALSIZE])    # [ BATCHSIZE x SEQLEN, INTERNALSIZE ]
-Ylogits = layers.linear(Yflat, ALPHASIZE)     # [ BATCHSIZE x SEQLEN, ALPHASIZE ]
-Yflat_ = tf.reshape(Yo_, [-1, ALPHASIZE])     # [ BATCHSIZE x SEQLEN, ALPHASIZE ]
-loss = tf.nn.softmax_cross_entropy_with_logits(logits=Ylogits, labels=Yflat_)  # [ BATCHSIZE x SEQLEN ]
+Y_flat = tf.reshape(Yr, [-1, INTERNALSIZE])    # [ BATCHSIZE x SEQLEN, INTERNALSIZE ]
+Ylogits = layers.linear(Y_flat, ALPHASIZE)     # [ BATCHSIZE x SEQLEN, ALPHASIZE ]
+Y_flat_ = tf.reshape(Yo_, [-1, ALPHASIZE])     # [ BATCHSIZE x SEQLEN, ALPHASIZE ]
+loss = tf.nn.softmax_cross_entropy_with_logits(logits=Ylogits, labels=Y_flat_)  # [ BATCHSIZE x SEQLEN ]
 loss = tf.reshape(loss, [batchsize, -1])      # [ BATCHSIZE, SEQLEN ]
 Yo = tf.nn.softmax(Ylogits, name='Yo')        # [ BATCHSIZE x SEQLEN, ALPHASIZE ]
 Y = tf.argmax(Yo, 1)                          # [ BATCHSIZE x SEQLEN ]
@@ -89,8 +77,8 @@ seqloss = tf.reduce_mean(loss, 1)
 batchloss = tf.reduce_mean(seqloss)
 accuracy = tf.reduce_mean(tf.cast(tf.equal(Y_, tf.cast(Y, tf.uint8)), tf.float32))
 loss_summary = tf.summary.scalar("batch_loss", batchloss)
-acc_summary = tf.summary.scalar("batch_accuracy", accuracy)
-summaries = tf.summary.merge([loss_summary, acc_summary])
+accuracy_summary = tf.summary.scalar("batch_accuracy", accuracy)
+summaries = tf.summary.merge([loss_summary, accuracy_summary])
 
 # Init Tensorboard stuff. This will save Tensorboard information into a different
 # folder at each run named 'log/<timestamp>/'. Two sets of data are saved so that
@@ -101,8 +89,8 @@ validation_writer = tf.summary.FileWriter("log/" + timestamp + "-validation")
 
 # Init for saving models. They will be saved into a directory named 'checkpoints'.
 # Only the last checkpoint is kept.
-if not os.path.exists("checkpoints3"):
-    os.mkdir("checkpoints3")
+if not os.path.exists("saves"):
+    os.mkdir("saves")
 saver = tf.train.Saver(max_to_keep=1000)
 
 # for display: init the progress bar
@@ -110,7 +98,7 @@ DISPLAY_FREQ = 50
 _50_BATCHES = DISPLAY_FREQ * BATCHSIZE * SEQLEN
 progress = txt.Progress(DISPLAY_FREQ, size=111+2, msg="Training on next "+str(DISPLAY_FREQ)+" batches")
 
-# init
+# initialize
 istate = np.zeros([BATCHSIZE, INTERNALSIZE*NLAYERS])  # initial zero input state
 init = tf.global_variables_initializer()
 sess = tf.Session()
@@ -118,7 +106,7 @@ sess.run(init)
 step = 0
 
 # training loop
-for x, y_, epoch in txt.rnn_minibatch_sequencer(codetext, BATCHSIZE, SEQLEN, nb_epochs=10):
+for x, y_, epoch in txt.rnn_minibatch_sequencer(code_text, BATCHSIZE, SEQLEN, nb_epochs=10):
 
     # train on one minibatch
     feed_dict = {X: x, Y_: y_, Hin: istate, lr: learning_rate, pkeep: dropout_pkeep, batchsize: BATCHSIZE}
@@ -136,11 +124,11 @@ for x, y_, epoch in txt.rnn_minibatch_sequencer(codetext, BATCHSIZE, SEQLEN, nb_
     # The validation text should be a single sequence but that's too slow (1s per 1024 chars!),
     # so we cut it up and batch the pieces (slightly inaccurate)
     # tested: validating with 5K sequences instead of 1K is only slightly more accurate, but a lot slower.
-    if step % _50_BATCHES == 0 and len(valitext) > 0:
+    if step % _50_BATCHES == 0 and len(validation_text) > 0:
         VALI_SEQLEN = 1*1024  # Sequence length for validation. State will be wrong at the start of each sequence.
-        bsize = len(valitext) // VALI_SEQLEN
-        txt.print_validation_header(len(codetext), bookranges)
-        vali_x, vali_y, _ = next(txt.rnn_minibatch_sequencer(valitext, bsize, VALI_SEQLEN, 1))  # all data in 1 batch
+        bsize = len(validation_text) // VALI_SEQLEN
+        txt.print_validation_header(len(code_text), bookranges)
+        vali_x, vali_y, _ = next(txt.rnn_minibatch_sequencer(validation_text, bsize, VALI_SEQLEN, 1))  # all data in 1 batch
         vali_nullstate = np.zeros([bsize, INTERNALSIZE*NLAYERS])
         feed_dict = {X: vali_x, Y_: vali_y, Hin: vali_nullstate, pkeep: 1.0,  # no dropout for validation
                      batchsize: bsize}
@@ -166,7 +154,7 @@ for x, y_, epoch in txt.rnn_minibatch_sequencer(codetext, BATCHSIZE, SEQLEN, nb_
         saved_file = saver.save(sess, 'checkpoints3/rnn_train_' + timestamp, global_step=step)
         print("Saved file: " + saved_file)
 
-    # display progress bar
+    # progress bar
     progress.step(reset=step % _50_BATCHES == 0)
 
     # loop state around
